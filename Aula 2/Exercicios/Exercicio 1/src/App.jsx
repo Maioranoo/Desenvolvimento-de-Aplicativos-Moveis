8ab9aab883a9c1d7148846795664c7bcd2878404
Maioranoo <jp.maioranoo@gmail.com>
2026-08-24 22:04:10 -0300

    Criação do laboratorio em react

---
+++ Aula 2/Exercicios/Exercicio 1/src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div>
+      <div style={{ backgroundColor: '#add8e6', padding: '20px' }}>
+        <h1>Computação Móvel</h1>
+        <h1>React</h1>
+        <p>Biblioteca JavaScript para criar UI</p>
+        <p>Aula 1</p>
+      </div>
+
+      <div style={{ backgroundColor: '#90ee90', padding: '20px' }}>
+        <h2>Vamos utilizar</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
